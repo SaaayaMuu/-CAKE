2bcf0de70cee510f85ab921765d0ee453d101829
SaaayaMuu <saya322.coco@gmail.com>
2022-06-27 13:22:47 +0000

    [Add]admin全体レイアウト

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,11 @@
 class Public::CustomersController < ApplicationController
   
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def unsubscribe
